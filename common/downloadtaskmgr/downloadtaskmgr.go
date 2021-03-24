b2a10839b97b7c8083a346a6a24ba2f840fb47a3
goodboy3 <zhangzhengbo@hotmail.com>
2021-03-24 19:24:35 +0800

    temp commit

--- common/downloadtaskmgr/downloadtaskmgr.go
+++ common/downloadtaskmgr/downloadtaskmgr.go
@@ -55,6 +55,9 @@ var recordWriter *bufio.Writer
 
 var jobChan = make(chan *DownloadTask, 1024)
 
+var speedLimit = []int64{0, 10 * 1e3, 50 * 1e3, 200 * 1e3, 1000 * 1e3, 1000 * 1e6} //download channel speed line [0 KB/s, 10KB/s, 50KB/s, 200KB/s, 1000KB/s, 1000MB/s]
+var countChan = make(chan bool, 5)
+
 func InitTaskMgr(rootPath string) {
 	if !utils.Exists(rootPath) {
 		err := os.Mkdir(rootPath, 0700)
@@ -74,6 +77,11 @@ func InitTaskMgr(rootPath string) {
 		fileHandle.Close()
 		fileHandle = nil
 	}()
+
+	//allow 5 tasks in same time
+	for i := 0; i < 5; i++ {
+		countChan <- true
+	}
 
 	//read unfinished task
 	reader := bufio.NewReader(fileHandle)
@@ -159,23 +167,28 @@ func Run() {
 					logger.Error("execFun is nil, no func to exec task")
 					return
 				}
-				err := execFunc(task)
-				if err != nil {
-					//task failed
-					//push to the end of the list
-					if task.TryTimes < 3 {
-						tryTimes := task.TryTimes + 1
-						AddTask(task.TargetUrl, task.OriginTag, task.Continent, task.Country, task.Area, task.BindNameHash, task.FileNameHash, tryTimes)
-					} else {
-						if onTaskFailed != nil {
-							onTaskFailed(task)
+				go func() {
+					<-countChan
+					err := execFunc(task)
+					countChan <- true
+					if err != nil {
+						//task failed
+						//push to the end of the list
+						if task.TryTimes < 3 {
+							tryTimes := task.TryTimes + 1
+							AddTask(task.TargetUrl, task.OriginTag, task.Continent, task.Country, task.Area, task.BindNameHash, task.FileNameHash, tryTimes)
+						} else {
+							if onTaskFailed != nil {
+								onTaskFailed(task)
+							}
 						}
-					}
 
-				}
-				leftTaskCount--
-				//delete task from record file
-				RemoveFinishedTaskFromFile()
+					}
+					leftTaskCount--
+					//delete task from record file
+					RemoveFinishedTaskFromFile()
+				}()
+
 			}
 		}
 	}()
@@ -243,14 +256,21 @@ func TimeoutDialer(cTimeout time.Duration, rwTimeout time.Duration) func(net, ad
 		if err != nil {
 			return nil, err
 		}
-		conn.SetDeadline(time.Now().Add(rwTimeout))
+		if rwTimeout > 0 {
+			err := conn.SetDeadline(time.Now().Add(rwTimeout))
+			if err != nil {
+				logger.Error("set download process rwTimeout error", "err", err)
+				return nil, err
+			}
+		}
 		return conn, nil
 	}
 }
 
 func DownLoadFile(url string, distFilePath string) error {
 	connectTimeout := 10 * time.Second
-	readWriteTimeout := 3600 * 3 * time.Second
+	//readWriteTimeout := 3600 * 3 * time.Second
+	readWriteTimeout := time.Duration(0)
 	//http client
 	c := http.Client{
 		Transport: &http.Transport{
@@ -285,7 +305,69 @@ func DownLoadFile(url string, distFilePath string) error {
 		return errors.New("body is null")
 	}
 	defer response.Body.Close()
-	_, err = io.Copy(file, response.Body)
+
+	//buff := make([]byte, 32*1024)
+	//written := 0
+	//writer := bufio.NewWriter(file)
+	//reader := bufio.NewReaderSize(response.Body, 1024*32)
+	//go func() {
+	//	for {
+	//		nr, er := reader.Read(buff)
+	//		if nr > 0 {
+	//			nw, ew := writer.Write(buff[0:nr])
+	//			if nw > 0 {
+	//				written += nw
+	//			}
+	//			if ew != nil {
+	//				err = ew
+	//				break
+	//			}
+	//			if nr != nw {
+	//				err = io.ErrShortWrite
+	//				break
+	//			}
+	//		}
+	//		if er != nil {
+	//			if er != io.EOF {
+	//				err = er
+	//			}
+	//			break
+	//		}
+	//	}
+	//	if err != nil {
+	//		logger.Error("download file error","err",err)
+	//	}
+	//}()
+	//
+	//spaceTime := time.Second * 1
+	//ticker := time.NewTicker(spaceTime)
+	//lastWtn := 0
+	//stop := false
+	//
+	//for {
+	//	select {
+	//	case <-ticker.C:
+	//		speed := written - lastWtn
+	//		fmt.Printf("[*] Speed %s / %s \n", speed, spaceTime.String())
+	//		if written-lastWtn == 0 {
+	//			ticker.Stop()
+	//			stop = true
+	//			break
+	//		}
+	//		lastWtn = written
+	//	}
+	//	if stop {
+	//		break
+	//	}
+	//}
+
+	//_, err = io.Copy(file, response.Body)
+
+	//get a speed limit
+	speedLimit := 9999999
+
+	_, err = copyBuffer2(file, response.Body, nil, int64(speedLimit))
+
 	if err != nil {
 		os.Remove(distFilePath)
 		return err
@@ -308,4 +390,146 @@ func DownLoadFile(url string, distFilePath string) error {
 	}
 
 	return nil
+}
+
+func copyBuffer2(dst io.Writer, src io.Reader, buf []byte, speedLimit int64) (written int64, err error) {
+	// If the reader has a WriteTo method, use it to do the copy.
+	// Avoids an allocation and a copy.
+	//if wt, ok := src.(io.WriterTo); ok {
+	//	return wt.WriteTo(dst)
+	//}
+	//// Similarly, if the writer has a ReadFrom method, use it to do the copy.
+	//if rt, ok := dst.(io.ReaderFrom); ok {
+	//	return rt.ReadFrom(src)
+	//}
+	if buf == nil {
+		size := 32 * 1024
+		if l, ok := src.(*io.LimitedReader); ok && int64(size) > l.N {
+			if l.N < 1 {
+				size = 1
+			} else {
+				size = int(l.N)
+			}
+		}
+		buf = make([]byte, size)
+	}
+	//written:=int64(0)
+	stop := false
+	go func() {
+		for {
+			nr, er := src.Read(buf)
+			if nr > 0 {
+				nw, ew := dst.Write(buf[0:nr])
+				if nw > 0 {
+					written += int64(nw)
+				}
+
+				if ew != nil {
+					err = ew
+					break
+				}
+				if nr != nw {
+					err = io.ErrShortWrite
+					break
+				}
+			}
+			if er != nil {
+				if er != io.EOF {
+					err = er
+				}
+				break
+			}
+		}
+		stop = true
+	}()
+
+	spaceTime := time.Millisecond * 200
+	ticker := time.NewTicker(spaceTime)
+	lastWtn := int64(0)
+	lowSpeedCount := 0
+	for {
+		select {
+		case <-ticker.C:
+			speed := (written - lastWtn) * 5
+			fmt.Printf("[*] Speed %d / %s \n", speed, spaceTime.String())
+			if speed < speedLimit {
+				lowSpeedCount++
+			} else {
+				lowSpeedCount = 0
+			}
+			if lowSpeedCount > 25 {
+				ticker.Stop()
+				logger.Error("download speed low", "speed", speed)
+				return written, errors.New("slow download speed")
+			}
+
+			lastWtn = written
+		}
+		if stop {
+			break
+		}
+	}
+
+	return written, err
+}
+
+func copyBuffer(dst io.Writer, src io.Reader, buf []byte, speedLimit int64) (written int64, err error) {
+	// If the reader has a WriteTo method, use it to do the copy.
+	// Avoids an allocation and a copy.
+	//if wt, ok := src.(io.WriterTo); ok {
+	//	return wt.WriteTo(dst)
+	//}
+	//// Similarly, if the writer has a ReadFrom method, use it to do the copy.
+	//if rt, ok := dst.(io.ReaderFrom); ok {
+	//	return rt.ReadFrom(src)
+	//}
+	if buf == nil {
+		size := 32 * 1024
+		if l, ok := src.(*io.LimitedReader); ok && int64(size) > l.N {
+			if l.N < 1 {
+				size = 1
+			} else {
+				size = int(l.N)
+			}
+		}
+		buf = make([]byte, size)
+	}
+
+	// startTime
+	startTime := time.Now().UnixNano()
+	usedTime := float64(0)
+	speed := float64(0)
+	for {
+		nr, er := src.Read(buf)
+		if nr > 0 {
+			nw, ew := dst.Write(buf[0:nr])
+			if nw > 0 {
+				written += int64(nw)
+			}
+			usedTime = float64((time.Now().UnixNano() - startTime) / int64(time.Millisecond))
+			if usedTime > 3000 {
+				speed = float64(written) / float64(usedTime) // KB/s
+				logger.Debug("download speed", "speed", speed)
+				if speed < float64(speedLimit) {
+					return written, errors.New("download speed slow")
+				}
+			}
+
+			if ew != nil {
+				err = ew
+				break
+			}
+			if nr != nw {
+				err = io.ErrShortWrite
+				break
+			}
+		}
+		if er != nil {
+			if er != io.EOF {
+				err = er
+			}
+			break
+		}
+	}
+	return written, err
 }
