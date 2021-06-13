dc3137ac054351e8e362019b7f056afa5a33882e
goodboy3 <zhangzhengbo@hotmail.com>
2021-06-13 08:55:40 +0800

    commit

--- common/utils/utils.go
+++ common/utils/utils.go
@@ -348,8 +348,27 @@ func RandString(len int) string {
 	return string(bytes)
 }
 
-func chunkSlice(slice []interface{}, chunkSize int) [][]interface{} {
+func ChunkSlice(slice []interface{}, chunkSize int) [][]interface{} {
 	var chunks [][]interface{}
+	for {
+		if len(slice) == 0 {
+			break
+		}
+
+		// necessary check to avoid slicing beyond
+		// slice capacity
+		if len(slice) < chunkSize {
+			chunkSize = len(slice)
+		}
+
+		chunks = append(chunks, slice[0:chunkSize])
+		slice = slice[chunkSize:]
+	}
+	return chunks
+}
+
+func ChunkSliceString(slice []string, chunkSize int) [][]string {
+	var chunks [][]string
 	for {
 		if len(slice) == 0 {
 			break
